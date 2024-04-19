ff4a6e1925a772ae5e04d0c2236bd7f83daff91e
Sarah Brager <sarahbrager@utexas.edu>
2024-04-19 18:46:22 -0500

    first project check-in

--- src/js/main.js
+++
@@ -1,4 +0,0 @@
-// populate copyright in footer with current year
-const copyrightYear = document.querySelector('.copyright-year');
-const now = new Date();
-copyrightYear.innerHTML = now.getFullYear();
